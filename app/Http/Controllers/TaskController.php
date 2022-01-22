e9a6e02b75ecf78895b90a87b91230b2616d2ebe
algins <igors.algins@gmail.com>
2022-01-22 20:12:51 +0200

    use exact filters for task filtering

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -9,6 +9,7 @@ use App\Models\TaskStatus;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
+use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\QueryBuilder;
 
 class TaskController extends Controller
@@ -21,7 +22,11 @@ class TaskController extends Controller
     public function index(): View
     {
         $tasks = QueryBuilder::for(Task::class)
-            ->allowedFilters(['status_id', 'created_by_id', 'assigned_to_id'])
+            ->allowedFilters(
+                AllowedFilter::exact('status_id'),
+                AllowedFilter::exact('created_by_id'),
+                AllowedFilter::exact('assigned_to_id')
+            )
             ->get();
 
         $taskStatuses = TaskStatus::all();
