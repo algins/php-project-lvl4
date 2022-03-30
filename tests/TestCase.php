61f25cacb00bb57921c778b647d600747771f215
algins <igors.algins@gmail.com>
2022-03-30 16:19:14 +0300

    Use RefreshDatabase trait

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,14 +4,12 @@ namespace Tests;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use DatabaseTransactions;
-    use DatabaseMigrations;
     use CreatesApplication;
+    use RefreshDatabase;
 
     protected function setUp(): void
     {
